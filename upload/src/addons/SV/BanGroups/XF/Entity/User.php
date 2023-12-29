db51843bc1c92d760b2341c73e5d4643eac519f9
Xon <635541+Xon@users.noreply.github.com>
2023-12-29 19:44:53 +0800

    Cleanup & fix wrong group being used on ban-rebuild

--- upload/src/addons/SV/BanGroups/XF/Entity/User.php
+++ upload/src/addons/SV/BanGroups/XF/Entity/User.php
@@ -61,41 +61,50 @@ class User extends XFCP_User
         $options = \XF::options();
         $userId = $this->user_id;
 
-        $rejectGroup = (int)($options->sv_addRejectUserGroup ?? 0);
-        if ($rejectGroup !== 0 && $rejectionChange === 'enter')
+        if ($rejectionChange !== null)
         {
-            $userGroupChangeService->addUserGroupChange($userId, 'svRejectedUserGroup', $rejectGroup);
-        }
-        else if ($rejectionChange !== null)
-        {
-            $userGroupChangeService->removeUserGroupChange($userId, 'svRejectedUserGroup');
-        }
-
-        $disableGroup = (int)($options->sv_addDisableUserGroup ?? 0);
-        if ($disableGroup !== 0 && $disabledChange === 'enter')
-        {
-            $userGroupChangeService->addUserGroupChange($userId, 'svDisabledUserGroup', $disableGroup);
-        }
-        else if ($disabledChange !== null)
-        {
-            $userGroupChangeService->removeUserGroupChange($userId, 'svDisabledUserGroup');
+            $rejectGroup = (int)($options->sv_addRejectUserGroup ?? 0);
+            if ($rejectGroup !== 0 && $rejectionChange === 'enter')
+            {
+                $userGroupChangeService->addUserGroupChange($userId, 'svRejectedUserGroup', $rejectGroup);
+            }
+            else
+            {
+                $userGroupChangeService->removeUserGroupChange($userId, 'svRejectedUserGroup');
+            }
         }
 
-        if ($this->Ban !== null && !$this->Ban->end_date)
+        if ($disabledChange !== null)
         {
-            $banGroupId = (int)($options->sv_addBanUserGroupPerm ?? 0);
+            $disableGroup = (int)($options->sv_addDisableUserGroup ?? 0);
+            if ($disableGroup !== 0 && $disabledChange === 'enter')
+            {
+                $userGroupChangeService->addUserGroupChange($userId, 'svDisabledUserGroup', $disableGroup);
+            }
+            else
+            {
+                $userGroupChangeService->removeUserGroupChange($userId, 'svDisabledUserGroup');
+            }
         }
-        else
+
+        if ($banChanged !== null)
         {
-            $banGroupId = (int)($options->addBanUserGroup ?? 0);
-        }
-        if ($banGroupId !== 0 && $banChanged === 'enter')
-        {
-            $userGroupChangeService->addUserGroupChange($userId, 'banGroup', $disableGroup);
-        }
-        else if ($banChanged !== null)
-        {
-            $userGroupChangeService->removeUserGroupChange($userId, 'banGroup');
+            if ($this->Ban !== null && !$this->Ban->end_date)
+            {
+                $banGroupId = (int)($options->sv_addBanUserGroupPerm ?? 0);
+            }
+            else
+            {
+                $banGroupId = (int)($options->addBanUserGroup ?? 0);
+            }
+            if ($banGroupId !== 0 && $banChanged === 'enter')
+            {
+                $userGroupChangeService->addUserGroupChange($userId, 'banGroup', $banGroupId);
+            }
+            else
+            {
+                $userGroupChangeService->removeUserGroupChange($userId, 'banGroup');
+            }
         }
     }
 }
